8648bf9631cf20df513627ee09939a1253b19330
JavierBorrero <javi.borrero.cerro@gmail.com>
2024-07-23 20:41:08 +0200

    hello world

--- Csharp-part-1/write-first-cs-code.cs
+++ Csharp-part-1/write-first-cs-code.cs
@@ -3,7 +3,18 @@ using System;
 public class HelloWorld {
 
     static void Main(){
+        // Imprime Hello world con un salto de linea al final
         Console.WriteLine("Hello world");
+
+        // Imprime los mensajes en la misma linea
+        Console.Write("Congratulations!");
+        Console.Write(" ");
+        Console.Write("You wrote your first lines of code");
+
+        // Con y sin salto de linea
+        Console.WriteLine("Congratulations");
+        Console.Write("You wrote your first lines of code");
+
     }
     
 }
